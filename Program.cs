4e4079a24e090d5880ae5b5878172b1b2d91158a
FruityBacon <amy.surmann.5752@gmail.com>
2024-06-20 19:02:58 +0200

    Konzolos input-output kezelés, program magyarázó lehetőség, titkosító osztály készítése

--- Program.cs
+++ Program.cs
@@ -1,8 +1,52 @@
-﻿namespace titkosito;
+﻿using System;
+using titkosito;
+
+namespace titkosito;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Nem volt megadva művelet.");
+            Console.WriteLine("Segítségért használja a -h müveletet!");
+            return;
+        }
+        Secret titkosito = new();
+        try
+        {
+            if (args.Length != 3 && args[0] != "-h")
+            {
+                throw new Exception("Túl "+((args.Length > 3)? "sok" : "kevés")+" érték volt megadva!");
+            }
+
+            switch (args[0])
+            {
+                case "-e" : 
+                    // Encryption, avagy titkosítás. Sikeres titkosítás esetén kettő értéket ad vissza üzenetet és kulcsot
+                    (string message,string key) eredmeny = titkosito.Encrypt(args[1],args[2]);
+                    Console.WriteLine("{0}\n{1}",eredmeny.message,eredmeny.key);
+                    break;
+
+                case "-d" :
+                    // Decryption, avagy visszafejtés. Sikeres visszafejtés esetén egy értéket ad vissza, ami az eredeti üzenet
+                    
+                    Console.WriteLine("{0}",titkosito.Decrypt(args[1],args[2]));
+                    break;
+                case "-h" :
+                    Console.WriteLine(" _   _ _   _             _ _     \n| |_(_) |_| | _____  ___(_) |_ ___  \n| __| | __| |/ / _ \\/ __| | __/ _ \\ \n| |_| | |_|   < (_) \\__ \\ | || (_) |\n \\__|_|\\__|_|\\_\\___/|___/_|\\__\\___/ ");
+                    Console.WriteLine(titkosito.Help());
+                    return;
+                default:
+                    throw new Exception("Kért művelet nem létezik: "+args[0]);
+            }
+        }
+        catch (System.Exception e)
+        {
+            Console.WriteLine("Hiba: {0}",e.Message);
+            return;
+        }
+        
     }
 }
---
+++ Secret.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+
+namespace titkosito;
+public class Secret
+{
+    private Dictionary<char,int> charvals = new Dictionary<char, int>() {
+        {'a',0},
+        {'b',1},
+        {'c',2},
+        {'d',3},
+        {'e',4},
+        {'f',5},
+        {'g',6},
+        {'h',7},
+        {'i',8},
+        {'j',9},
+        {'k',10},
+        {'l',11},
+        {'m',12},
+        {'n',13},
+        {'o',14},
+        {'p',15},
+        {'q',16},
+        {'r',17},
+        {'s',18},
+        {'t',19},
+        {'u',20},
+        {'v',21},
+        {'w',22},
+        {'x',23},
+        {'y',24},
+        {'z',25},
+        {' ',26},
+    };
+    public (string message, string key) Encrypt(string message, string key)
+    {
+        return ("asd","asd");
+    }
+
+    public string Decrypt(string smessage, string key)
+    {
+        return "blabla";
+    }
+
+    public string Help()
+    {
+        
+        return 
+        "Készítette Kiss Máté\n\nLeírás:\n\t\nMűveletek:\n\t-e [ÜZENET] [KULCS]\n\t-d [TITKOSÍTOTT ÜZENET] [KULCS]\n\t-h\n\nMagyarázat:\n\t-e \tTitkosítás, adja meg a titkosítani kívánt üznetet és hozzá a kulcsot.\n\t-d\tVisszafjetés, adja meg a titkosított üzenetet és utána a kulcsot.\n\t-h\tSegítség előhívása.";
+    }
+}
