45478c528778bb851f8fdbc086660f6fbf44bece
FruityBacon <amy.surmann.5752@gmail.com>
2024-06-22 16:45:07 +0200

    Jelszó kitaláló elkezdése. Szó kereső függvény összehasonlítás egyszerűsítéséhez. Angol zó gyüjtemény hoszzáadása.

--- Secret.cs
+++ Secret.cs
@@ -1,10 +1,12 @@
 using System;
+using System.IO;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace titkosito;
 public class Secret
 {
-    private List<char> charvals = new List<char>(){
+    private readonly List<char> charvals = new List<char>(){
         'a',
         'b',
         'c',
@@ -33,6 +35,8 @@ public class Secret
         'z',
         ' '
     };
+
+    private List<string> words = new();
 
     public string Encrypt(string message, string key)
     { 
@@ -63,7 +67,7 @@ public class Secret
         for (int i = 0; i < message.Length; i++)
         {
             int add = charvals.IndexOf(message[i]) - charvals.IndexOf(key[i]);
-            if ( add < 0)
+            if (add < 0)
                 add = 27 - Math.Abs(add);
             
             output += charvals[add];
@@ -72,14 +76,51 @@ public class Secret
         return output;
     }
 
-    public string Crack(string msg1, string msg2)
+    public string[] Crack(string msg1, string msg2)
     {
-        return "belh";
+        try
+        {
+            StreamReader sr = new("words.txt");
+            while(!sr.EndOfStream)
+            {
+                words.Add(sr.ReadLine());
+            }
+            words.Sort();
+            sr.Close();
+        }
+        catch (System.Exception)
+        {
+            throw new Exception("Nem talált szó gyűjteményt.");
+        }
+
+        List<string> possibleKeys = new();
+        int keyLength = msg1.Length;
+
+        if (msg1.Length > msg2.Length)
+            keyLength = msg2.Length;
+
+        for (int i = 0; i < keyLength; i++)
+        {
+            
+        }
+
+        return possibleKeys.ToArray();
+    }
+
+    private string[] Finder(string inword) //Vissza ad egy tömböt az összes lehetséges szóval
+    {
+        List<string> output = new();
+        foreach(string word in words)
+        {
+            if (word.Contains(inword))
+                output.Add(word);
+        }
+        return output.ToArray();
     }
 
     public string Help()
     {
         return 
-        "Készítette Kiss Máté\n\nLeírás:\n\tAz angol abc betűivel írt üzenetek titkosítására és visszafejtésére alkalmas program.\n\nMűveletek:\n\t-c [TITKOS ÜZENET] [TITKOS ÜZENET]\n\t-e [ÜZENET] [KULCS]\n\t-d [TITKOSÍTOTT ÜZENET] [KULCS]\n\t-h\n\nMagyarázat:\n\t-c \tFeltörés, adjon meg kettő titkosított üzenetet, és vissza adja a lehetséges közös kulcsuk.\n\t-e \tTitkosítás, adja meg a titkosítani kívánt üznetet és hozzá a kulcsot.\n\t-d\tVisszafejtés, adja meg a titkosított üzenetet és utána a kulcsot.\n\t-h\tSegítség előhívása.";
+        "Készítette Kiss Máté\n\nLeírás:\n\tAz angol abc betűivel írt üzenetek titkosítására és visszafejtésére alkalmas program.\n\nMűveletek:\n\t-c [TITKOS ÜZENET] [TITKOS ÜZENET]\n\t-e [ÜZENET] [KULCS]\n\t-d [TITKOSÍTOTT ÜZENET] [KULCS]\n\t-h\n\nMagyarázat:\n\t-c \tFeltörés, adjon meg kettő titkosított üzenetet, és vissza adja a lehetséges közös kulcsukat.\n\t-e \tTitkosítás, adja meg a titkosítani kívánt üznetet és hozzá a kulcsot.\n\t-d\tVisszafejtés, adja meg a titkosított üzenetet és utána a kulcsot.\n\t-h\tSegítség előhívása.";
     }
 }
